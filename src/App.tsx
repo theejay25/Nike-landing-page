69ec8079c2c13b44fbb5735fb14e2771aefd5568
theejay25 <151005214+theejay25@users.noreply.github.com>
2025-05-18 15:42:29 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="capitalize text-white font-extrabold bg-black">here</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import './index.css'
+import App from './App.tsx'
+
+const router = createBrowserRouter([{
+  path: '/',
+  element:<App />,
+  errorElement: <div>404 not found</div>
+}])
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
