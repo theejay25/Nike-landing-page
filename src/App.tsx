61eece36cdaef1ebbdffba1e4159c515ee68e3b2
theejay25 <151005214+theejay25@users.noreply.github.com>
2025-05-18 16:10:55 +0100

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useEffect, useState } from "react"
 
 function App() {
 
+  const [open, setOpen] = useState(false)
+
+  useEffect(() => {
+    setOpen(true)
+  })
+
   return (
     <>
-      <h1 className="capitalize text-white font-extrabold bg-black">here</h1>
+      <div className={`h-0 w-0 t-0 l-0 uppercase font-extrabold text-5xl text-center duration-800 bg-black text-white ${open ? 'h-full w-full rounded-br-0' : 'rounded-br-[100%]'}`}>
+        <h1 className={`w-[100vw] text-center`}>here</h1>
+      </div>
     </>
   )
 }
