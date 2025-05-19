0244534e51ed4a56a55134222861b4b863013dc1
theejay25 <151005214+theejay25@users.noreply.github.com>
2025-05-19 14:49:06 +0100

    pain

--- src/components/OverlayPage.tsx
+++ src/components/OverlayPage.tsx
@@ -7,9 +7,11 @@ interface props {
 const OverlayPage = ({ Click }: props) => {
   return (
     <>
-    <div className="top-20 absolute h-full w-full z-10" onClick={Click}>
-        <p className="uppercase font-extrabold text-white text-4xl pl-9 mt-35">nike<br />air max</p>
-        <img src={Nike} alt="" className='w-[100vw] mt-[-15vh] ml-[22vw] rotate-320'/>
+    <div className="top-20 absolute h-full w-full z-10 " onClick={Click}>
+        <p className="title-text text-4xl pl-9 mt-35 lg:text-9xl">nike<br />air max</p>
+        <img src={Nike} alt="" className='shoe-image'/>
+        <p className="text-white w-[250px] pl-9 mt-[-25%] lg:mt-[-10%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum natus corporis porro.
+        </p>
     </div>
     </>
   )
